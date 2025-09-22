e8d2e20099f03eb1b5d4618f3ef4179f9d2b13e3
alvt1234 <villamila@unitec.edu>
2025-09-22 13:04:52 -0600

    ruta listar tours (no esta probada)

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -6,6 +6,23 @@ router.get('/test', (_req, res) => {
   res.json({ message: 'API is working!' });
 });
 
+/*	
+Devuelve una lista de tours limitada por el limit y offset.Ambos parámetros tienen un valor por defecto:
+Limit 10
+Offset 0*/
+router.get("/tours" ,async (req, res) =>{
+  const limit= parseInt(req.query.limit) || 10;
+  const offset= parseInt(req.query.offset) || 0;
 
+  try{
+    const sql=await pool.query("select * from tourapp.tours limit 10 offset 0;");
+    res.json(sql.rows);
+  }catch(err){
+    res.status(500).json("Eroor al listar los tours");
+  }
+} );
+/*	
+Devuelve la disponibilidad de los tours en base a la fecha actual, solamente de fechas 
+futuras.Tampoco devuelve las horas que ya han sido reservadas.*/
 
 module.exports = router;
